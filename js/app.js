86be292fd90bfbd73874718fc13c59e14b4f9322
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-20 10:49:23 +0100

    built base structure of homepage

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 console.log('js running');
 
 $(()=>{
-  new TypeIt('#introductionText', {
-    strings: 'Hey, I\'m Bianca',
-    speed: 50,
-    autoStart: true
-  });
+  // new TypeIt('#introductionText', {
+  //   strings: 'Hey, I\'m Bianca',
+  //   speed: 50,
+  //   autoStart: true
+  // });
 
-
+  // get this to work?
   $('.smooth').on('scroll', function() {
     $.smoothScroll({
       scrollElement: $('body'),
@@ -17,6 +17,21 @@ $(()=>{
     });
 
     return false;
+  });
+
+  $('i').on('click', function(e){
+    if(this.hash !== ''){
+      console.log(this.hash); 
+      e.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body'.animate({
+        scrollTop: $(hash).offset().top
+      },800, function(){
+        window.location.hash = hash;
+      }));
+    }
   });
 
 
